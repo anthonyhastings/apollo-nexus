cf557222d47ccda8f73738942561b4214cde64ed
Anthony Hastings <ar.hastings@gmail.com>
2022-08-01 11:37:45 +0100

    Adding test framework, docker updates and query/mutation tests

---
+++ server/jest-mjs-resolver.ts
@@ -0,0 +1,18 @@
+// @ts-nocheck
+
+const mjsResolver = (path, options) => {
+  const mjsExtRegex = /\.mjs$/i;
+  const resolver = options.defaultResolver;
+
+  if (mjsExtRegex.test(path)) {
+    try {
+      return resolver(path.replace(mjsExtRegex, '.mts'), options);
+    } catch {
+      // use default resolver
+    }
+  }
+
+  return resolver(path, options);
+};
+
+module.exports = mjsResolver;
